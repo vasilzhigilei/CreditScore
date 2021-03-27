7bc820ff7a97a9ec3bbfdf011361016ac6eddcf0
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-03-27 19:15:04 -0400

    demo page

--- creditscore/creditscore.js
+++ creditscore/creditscore.js
@@ -1,3 +1,8 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 
 function estimate(){
     // ALREADY 0-1 SCALE
