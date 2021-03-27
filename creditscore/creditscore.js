e8f22b9817771c6d7fd34552e2f8aaeeb48ea505
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2021-03-27 18:22:35 -0400

    initial js work

--- creditscore/creditscore.js
+++ creditscore/creditscore.js
@@ -1,8 +1,15 @@
 
 function estimate(){
-    var estimateModal = new bootstrap.Modal(document.getElementById('estimateModal'), {
-        keyboard: false,
-        backdrop: false,
-    });
-    estimateModal.toggle();
+    // ALREADY 0-1 SCALE
+    paymentsOnTime = document.querySelector('input[name="paymentRadioOptions"]:checked').value;
+    ageOfCredit = document.querySelector('input[name="ageRadioOptions"]:checked').value;
+    utilization = document.querySelector('input[name="utilizationRadioOptions"]:checked').value;
+
+    // NEED TO SCALE
+    balanceValue = document.getElementById('unpaidInput').value;
+    creditLimitValue = document.getElementById('totalLimitInput').value; // 20-22k excellent
+    
+
+    // We'll follow: credit utilization? 0-20%(Excellent), 21-30%(Good), 31-50%(Average), 51-100%(Poor)
+    balanceValue/creditLimitValue
 }
